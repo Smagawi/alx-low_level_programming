633fd869d54779c8893e00df2e5bcb682279b54b
Shane Magawi <magawishane@gmail.com>
2022-12-10 00:50:01 +0000

    smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,12 +6,14 @@
  */
 int main(void)
 {
-	char l;
+	char c;
 
-	for (l = 'z''; l >= 'a'; l--)
-		putchar(l);
-
+	c = 'z';
+	while
+		(c >= 'a')
+		{putchar(c);
+			c--;
+		}
 	putchar('\n');
-
-	return(0);
+	return (0);
 }
