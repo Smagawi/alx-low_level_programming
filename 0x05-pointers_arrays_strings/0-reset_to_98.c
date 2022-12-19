c2eb0d167731f85711138a0cc98497a2f70f345c
Shane Magawi <magawishane@gmail.com>
2022-12-19 23:28:37 +0300

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Resets the value of a variable back to 98.
+ *
+ * @n: int parameter
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
