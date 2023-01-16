cc54dde3346f6e82208353baa7096ce297fbde13
Shane Magawi <magawishane@gmail.com>
2023-01-16 13:13:51 +0300

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,17 +10,30 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i += 1)
+	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3) == 0)
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			if (i == 100)
+				printf("Buzz");
+			else
+				printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
 			printf("Fizz ");
-		else if ((i % 5) == 0)
-			printf("Buzz ");
-		else if (((i % 3) == 0) && ((i % 5) == 0))
-			printf("Fizzbuzz ");
+		}
 		else
+		{
 			printf("%d ", i);
+		}
 	}
+
 	printf("\n");
+
 	return (0);
 }
